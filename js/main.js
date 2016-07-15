5a4fbeaaf3f5b477afcd5ea6437aaeea3ffa3da9
opazisimo <girasolesporlavida@gmail.com>
2016-07-15 17:42:29 +0100

    initial commit

---
+++ js/main.js
@@ -0,0 +1,32 @@
+function mostrarAccion(){
+	$(".col-md-2").show();
+	$(".col-md-2").not(".accion").hide();
+}
+
+function mostrarComedia(){
+	$(".col-md-2").show();
+	$(".col-md-2").not(".comedia").hide();
+}
+
+function mostrarCrimen(){
+	$(".col-md-2").show();
+	$(".col-md-2").not(".crimen").hide();
+}
+
+function mostrarFamiliar(){
+	$(".col-md-2").show();
+	$(".col-md-2").not(".familiar").hide();
+}
+
+function mostrarTerror(){
+	$(".col-md-2").show();
+	$(".col-md-2").not(".terror").hide();
+}
+
+function mostrarTodas(){
+	$(".col-md-2").show();
+}
+
+$(document).ready(function(){
+	console.log("el yeicueri está cargaooo");
+});
